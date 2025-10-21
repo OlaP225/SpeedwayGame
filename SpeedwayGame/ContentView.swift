1ac21ba8363297f5018bbf13d19fb23e30487aa0
Ola <aleksandra.plichta@poczta.fm>
2025-10-21 18:36:52 +0200

    Feature: View complete

--- SpeedwayGame/ContentView.swift
+++ SpeedwayGame/ContentView.swift
@@ -7,9 +7,21 @@
 
 import SwiftUI
 
+struct Player: Identifiable, Equatable {
+    let id = UUID()
+    var firstName: String
+    var lastName: String
+    var imageName: String
+}
+
 struct ContentView: View {
     
-    @State private var players = [" Maciej Janowski", "Bartłomiej Kowalski", "Daniel Bewley"]
+    @State private var players = [
+        Player(firstName: "Maciej", lastName: "Janowski", imageName: "player1"),
+        Player(firstName: "Bartłomiej", lastName: "Kowalski", imageName: "player2"),
+        Player(firstName: "Daniel", lastName: "Bewley", imageName: "player3"),
+        Player(firstName: "Tai", lastName: "Wofinden", imageName: "player1")
+    ]
     @State private var editMode: EditMode = .inactive
     
     var body: some View {
@@ -18,7 +30,7 @@ struct ContentView: View {
             Image("background")
                 .resizable()
                 .edgesIgnoringSafeArea(.all)
-            Image("player1").clipShape(.circle)
+            Image("dashund2").clipShape(.circle)
                 .overlay(Circle().stroke(Color.white, lineWidth: 2))
                 .shadow(color: Color.white, radius: 3)
             Text("Bieg 1")
@@ -32,13 +44,41 @@ struct ContentView: View {
                 .padding(.trailing, 210)
             VStack{
                 List {
-                    ForEach(players, id: \.self) { player in
-                        Text(player)
+                    ForEach(players, id: \.id) { player in
+                        
+                        HStack{
+                            
+                            Image(player.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 80, height: 80)
+                                .clipShape(Circle())
+                                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+                                .shadow(radius: 5)
+                            Spacer()
+                            
+                            Text(player.firstName)
+                                .font(.title2)
+                                .foregroundColor(.black)
+                            Text(player.lastName)
+                                .font(.title2)
+                                .foregroundColor(.black)
+                        
+                            
+                            Spacer()
+                            Spacer()
+                        }
+                        .listRowBackground(Color.white.opacity(0.8))
+                        
+                    
                         
                     }
                 }
                 .environment(\.editMode, $editMode)
                 .scrollContentBackground(.hidden)
+                .background(Color.clear)
+                .frame(width: 400, height: 500)
+                .cornerRadius(20)
                 
                 
                 
