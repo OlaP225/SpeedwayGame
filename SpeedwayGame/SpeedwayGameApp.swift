ff83d3f14a9bc3c6eb290c30f34820ce43cd8b46
Ola <aleksandra.plichta@poczta.fm>
2025-10-21 16:47:56 +0200

    Initial Commit

---
+++ SpeedwayGame/SpeedwayGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedwayGameApp.swift
+//  SpeedwayGame
+//
+//  Created by Aleksandra Plichta on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpeedwayGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
