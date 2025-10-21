dac35839773a2099af3c5bf25eaa5f00a5ae50e2
Ola <aleksandra.plichta@poczta.fm>
2025-10-21 18:11:37 +0200

    Feature: Add background, list with players and their photos.

--- SpeedwayGame/ContentView.swift
+++ SpeedwayGame/ContentView.swift
@@ -8,15 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var players = [" Maciej Janowski", "Bartłomiej Kowalski", "Daniel Bewley"]
+    @State private var editMode: EditMode = .inactive
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            Image("player1").clipShape(.circle)
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                .shadow(color: Color.white, radius: 3)
+            Text("Bieg 1")
+                .padding(.all, 6)
+                .shadow(color: .white, radius: 20)
+                .cornerRadius(20)
+                .font(.largeTitle)
+                .foregroundColor(Color.white)
+                .bold()
+                .padding(.bottom, 650)
+                .padding(.trailing, 210)
+            VStack{
+                List {
+                    ForEach(players, id: \.self) { player in
+                        Text(player)
+                        
+                    }
+                }
+                .environment(\.editMode, $editMode)
+                .scrollContentBackground(.hidden)
+                
+                
+                
+                Button(action: { withAnimation {
+                    editMode = (editMode == .inactive) ? .active : .inactive
+                }
+                }) {
+                    Text(editMode == .inactive ? "Edytuj" : "Gotowe")
+                        .foregroundColor(.blue)
+                        .padding()
+                        .background(Color.white.opacity(0.8))
+                        .cornerRadius(10)
+                        .bold()
+                }
+            }
         }
-        .padding()
+        
+        
     }
+    func onMove( from source: IndexSet, to destination: Int){
+        players.move(fromOffsets: source, toOffset: destination)
+    }
+    
 }
 
 #Preview {
