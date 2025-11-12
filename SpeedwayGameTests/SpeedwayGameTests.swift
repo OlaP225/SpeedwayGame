50790cc55cc4065d84bb5c45c4e3e3fd725621c7
Ola <aleksandra.plichta@poczta.fm>
2025-11-12 08:54:02 +0100

    FEATURE: Create Unit Tests Bundle.

---
+++ SpeedwayGameTests/SpeedwayGameTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpeedwayGameTests.swift
+//  SpeedwayGameTests
+//
+//  Created by Aleksandra Plichta on 12/11/2025.
+//
+
+import Testing
+
+struct SpeedwayGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
